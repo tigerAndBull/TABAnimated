66b16beacb799535f9d467e57d1aec695d494096
tigerAndBull <tigerAndBull@foxmail.com>
2020-05-06 00:51:10 +0800

    add reset frame cell

---
+++ AnimatedDemo/AnimatedDemo/ViewController/TableView/UserCode/View/TableViewCell/ResetFrameTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ResetFrameTableViewCell.h
+//  AnimatedDemo
+//
+//  Created by tigerAndBull on 2020/4/25.
+//  Copyright © 2020 tigerAndBull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ResetFrameTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AnimatedDemo/AnimatedDemo/ViewController/TableView/UserCode/View/TableViewCell/ResetFrameTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ResetFrameTableViewCell.m
+//  AnimatedDemo
+//
+//  Created by tigerAndBull on 2020/4/25.
+//  Copyright © 2020 tigerAndBull. All rights reserved.
+//
+
+#import "ResetFrameTableViewCell.h"
+
+@implementation ResetFrameTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
